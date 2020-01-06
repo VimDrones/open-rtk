5708609b6867fc7ad8a6fd48c1c07660a2bab0bb
Huibean <huibean.luo@gmail.com>
2020-01-06 14:14:19 +0800

    debug

--- main.py
+++ main.py
@@ -20,10 +20,8 @@ oled = Oled(dev= dev)
 if dev:
     ip = list(map(int, socket.gethostbyname(socket.gethostname()).split("."))) 
 else:
-    ip = list(map(int, subprocess.check_output("hostname -I", shell = True ).decode("utf-8").split("."))) 
-
-print("ip", ip)
-
+    ip = list(map(int, subprocess.check_output("hostname -I | cut -d\' \' -f1", shell = True ).decode("utf-8").split("."))) 
+    
 # create a socket object
 serversocket = socket.socket(
 	        socket.AF_INET, socket.SOCK_STREAM) 
